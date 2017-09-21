b5902dd398dad33002c5ddcda6665a5590176668
Steven Huang <huangshu91@gmail.com>
2017-09-20 23:05:25 -0700

    add missing nez files

---
+++ WillRose/GameEngine.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Nez;
+
+namespace WillRose
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class GameEngine : Core
+    {
+
+        Scene testlevel;
+
+        Scene testlevel_physics;
+
+        public GameEngine() : base()
+        {
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+
+            testlevel = Scene.createWithDefaultRenderer(Color.CornflowerBlue);
+
+            testlevel_physics = Scene.createWithDefaultRenderer(Color.Beige);
+
+            Core.scene = testlevel;
+        }
+        
+    }
+}
